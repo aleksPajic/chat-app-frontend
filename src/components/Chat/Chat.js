552d605ed61b407c8f850a4d15689c258b663666
Aleksandra Pajic <aleksandra.pajic.96@gmail.com>
2021-12-04 21:06:49 +0100

    Add generation command configuration and Chat and Join components

---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Chat.scss';
+
+const Chat = () => (
+  <div className="Chat">
+    Chat Component
+  </div>
+);
+
+Chat.propTypes = {};
+
+Chat.defaultProps = {};
+
+export default Chat;
---
+++ src/components/Join/Join.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Join.css';
+
+const Join = () => (
+  <div className="Join">
+    Join Component
+  </div>
+);
+
+Join.propTypes = {};
+
+Join.defaultProps = {};
+
+export default Join;
